39da6a93d3cac4b0b51eba021cd8932ca1b899b2
yiffparty.nx <yep333pingas@proton.me>
2025-04-29 00:15:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const products = [
-    { name: "Archivo 1", url: "https://example.com/archivo1.zip" },
+    { name: "Archivo 1", url: "https://archive.org/download/nspgames/Picross%20S%20Genesis%20and%20Master%20System%20Edition%20%5B010089701567A000%5D%5Bv0%5D.nsp" },
     { name: "Archivo 2", url: "https://example.com/archivo2.zip" },
     { name: "Archivo 3", url: "https://example.com/archivo3.zip" }
 ];
